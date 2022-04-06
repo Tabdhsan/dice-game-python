8b2e49bc1503334d864462e7c0fa248ef071da3a
Tabish <tabdhsan11@gmail.com>
2022-04-05 22:00:07 -0400

    updated game

--- DiceGame.py
+++ DiceGame.py
@@ -16,19 +16,34 @@ class Player:
     def clear_score(self):
         self.total_score = 0
 
+    def add_to_total(self, die1, die2):
+        self.turn_total += die1 + die2
+
+    def snake_eyes_rolled(self):
+        self.total_score = 0
+        self.turn_total = 0
+        print(
+            f"{self.name} rolled Snake Eyes. Their Turn Total is now 0 and their Total Score is now 0"
+        )
+
+    def single_one_rolled(self):
+        self.turn_total = 0
+        print(
+            f"{self.name} rolled a 1, their turn total is now 0 and total score is {self.total_score}"
+        )
+
+    # Updates total score and sets turn total to 0
+    def end_turn(self):
+        self.total_score += self.turn_total
+        self.turn_total = 0
+        print(
+            f"{self.name}'s Turn Has Ended.\nTheir current Total Score is {self.total_score}\n"
+        )
+
 
 ## Functions ##
 def dice_roll():
     return random.randint(1, 6), random.randint(1, 6)
-
-
-def no_ones_or_doubles(die1, die2):
-    return die1 != 1 and die2 != 1 and die1 != die2
-
-
-def check_for_valid_doubles(die1, die2):
-    # Snake Eyes are not considered valid doubles
-    return die1 == die2 and die1 + die2 != 2
 
 
 def name_input_validation(player_input):
@@ -49,7 +64,7 @@ def player_choice_validation(player, player_input):
 
     # Runs correct function (next move) based on choice
     if input_cleanup == "HOLD":
-        end_turn(player)
+        player.end_turn()
     elif input_cleanup == "CONTINUE":
         print(f"{player.name} chose to continue")
         player_turn(player)
@@ -62,58 +77,33 @@ def player_choice_validation(player, player_input):
         player_choice_validation(player, player_answer)
 
 
-# Checks if a one was rolled and how many
-def one_rolled(player, die1, die2):
-
-    # Checks for "Snake Eyes"
-    if die1 == 1 and die2 == 1:
-        player.total_score = 0
-        player.turn_total = 0
-        print(
-            f"{player.name} rolled Snake Eyes. Their Turn Total is now 0 and their Total Score is now 0"
-        )
-        end_turn(player)
-
-    # Checks if one was rolled
-    elif die1 == 1 or die2 == 1:
-        player.turn_total = 0
-        print(
-            f"{player.name} rolled a 1, their turn total is now 0 and total score is {player.total_score}"
-        )
-        end_turn(player)
-
-
 def player_turn(player):
     # Roll Logic
     die1, die2 = dice_roll()
     print_dice(die1, die2)
     print(f"{player.name} just rolled a [{die1}] and a [{die2}]")
 
-    if no_ones_or_doubles(die1, die2):
-        # Asks player for choice for next move
-        player.turn_total += die1 + die2
-        print(f"{player.name}'s turn total is {player.turn_total}")
-        player_choice = input(f'Would {player.name} Like To "Hold" or "Continue"?: ')
-        player_choice_validation(player, player_choice)
+    if die1 == 1 and die2 == 1:
+        player.snake_eyes_rolled()
+        player.end_turn()
 
-    elif check_for_valid_doubles(die1, die2):
-        # Forces another roll
-        player.turn_total += die1 + die2
+    elif die1 == 1 or die2 == 1:
+        # Checks if only one one was rolled
+        player.single_one_rolled()
+        player.end_turn()
+
+    elif die1 == die2:
+        player.add_to_total(die1, die2)
         print(f"Because {player.name} rolled doubles, they must go again!")
+        # Forces another roll
         player_turn(player)
 
     else:
-        # One has been rolled
-        one_rolled(player, die1, die2)
-
-
-# Updates total score and sets turn total to 0
-def end_turn(player):
-    player.total_score += player.turn_total
-    player.turn_total = 0
-    print(
-        f"{player.name}'s Turn Has Ended.\nTheir current Total Score is {player.total_score}\n"
-    )
+        player.add_to_total(die1, die2)
+        # Asks player for choice for next move
+        print(f"{player.name}'s turn total is {player.turn_total}")
+        player_choice = input(f'Would {player.name} Like To "Hold" or "Continue"?: ')
+        player_choice_validation(player, player_choice)
 
 
 def play_game():
@@ -132,7 +122,7 @@ def play_game():
         print(f"Both players names cannot be the same. Player 2 is now {p2.name}")
 
     # Runs until someone reaches [WINNING_SCORE] points
-    while p1.total_score <= WINNING_SCORE and p2.total_score <= WINNING_SCORE:
+    while p1.total_score < WINNING_SCORE and p2.total_score < WINNING_SCORE:
         player_turn(p1)
         # Checks to see if player one has won before player 2's turn
         if p1.total_score < WINNING_SCORE:
@@ -141,8 +131,8 @@ def play_game():
     # Announces winner (Congrats for getting this far in the code! Here's a cookie 🍪)
     winner = p1.name if p1.total_score > p2.total_score else p2.name
     print(f"Game has ended. {winner} is the winner! :)")
-    print(p1.total_score)
-    print(p2.total_score)
+    print(f"{p1.name} final score is {p1.total_score}")
+    print(f"{p2.name} final score is {p2.total_score}")
 
 
 play_game()
